ae9ae4d3d4115073e9479ada1668f8b1006479b3
Dru <drusademumba@gmail.com>
2023-04-24 09:08:50 -0700

    Add script that prints combination of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,37 @@
 #include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print combination of 3 digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (n = 0; n < 10; n++)
+			{
+				if (i != n && i != j && j != n && i < j && j < n)
+				{
+					putchar('0' | i);
+					putchar('0' | j);
+					putchar('0' | n);
+
+					if (i + j + n != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
