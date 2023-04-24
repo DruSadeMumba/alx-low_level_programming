3983f97004edf3bd928b9e3c7921209de20d7ef4
Dru <drusademumba@gmail.com>
2023-04-24 08:48:33 -0700

    Add script to print combination of 2 digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,33 @@
 #include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print combination of 2 digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i != j && i < j)
+			{
+				putchar('0' | i);
+				putchar('0' | j);
+
+				if (i + j != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
