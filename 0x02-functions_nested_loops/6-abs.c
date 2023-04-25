a0667d290d72e8d1dbd4a67f9636eb5316443a7e
Dru <drusademumba@gmail.com>
2023-04-25 09:21:32 -0700

    Add script to print absolute value of int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - entry
+ *
+ * Description: absolute value of int
+ * @n: integer
+ *
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+	}
+	return (n);
+}
