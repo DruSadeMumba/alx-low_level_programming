8dccee88ac7e48bcda51df4dfb664d30e15abff0
Dru <drusademumba@gmail.com>
2023-04-25 11:29:13 -0700

    Add script to add 2 integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - entry
+ *
+ * Description: Add two integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
