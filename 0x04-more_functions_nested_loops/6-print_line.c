aaf95141196cce9fdd0d00ac77382df2f72a8337
Dru <drusademumba@gmail.com>
2023-04-27 05:21:41 -0700

    Add function to print a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - entry
+ *
+ * Description: PRint a line
+ * @n: integer
+ */
+void print_line(int n)
+{
+	int l;
+
+	for (l = 0; l < n; l++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
