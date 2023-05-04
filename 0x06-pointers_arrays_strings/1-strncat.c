d2186c62040377ba2185f0d51c77adc1e001cefe
Dru <drusademumba@gmail.com>
2023-05-04 04:20:16 -0700

    add function that concatenates two strings upto n characters

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,28 @@
 #include "main.h"
+
+/**
+ * _strncat - concatenate two strings
+ *
+ * @src: string
+ * @dest: string
+ * @n: number of bytes
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	j = 0;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i + j] = src[i];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
