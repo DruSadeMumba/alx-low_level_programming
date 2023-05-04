6602123458e6e87405c6cebec52204a59fadb538
Dru <drusademumba@gmail.com>
2023-05-04 10:09:37 -0700

    Capitalise first letter of string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,38 @@
 #include "main.h"
+
+/**
+ * cap_string - change first letters to uppercase
+ *
+ * @s: first letter
+ *
+ * Return: char
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	i = 0;
+	while (*(s + i))
+	{
+		if (*(s + i) >= 97 && *(s + i) <= 122)
+		{
+			if (i == 0)
+			{
+				*(s + i) -= 97 - 65;
+			} else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+					{
+						*(s + i) -= 97 - 65;
+					}
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
+
