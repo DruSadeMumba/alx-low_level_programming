a802aea73318c868271bb0b9f679c83f33cc3bcd
Dru <drusademumba@gmail.com>
2023-05-04 13:19:13 -0700

    Write a function that prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,1 +1,41 @@
 #include "main.h"
+
+/**
+ * print_buffer - print a buffer
+ *
+ * @b: buffer
+ * @size: size
+ */
+void print_buffer(char *b, int size)
+{
+	int a, i;
+
+	for (a = 0; a < size; a += 10)
+	{
+		printf("%08x: ", a);
+
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + a) >= size)
+				printf(" ");
+			else
+				printf("%02x", *(b + i + a));
+			if ((i % 2) != 0 && i != 0)
+				printf(" ");
+		}
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + a) >= size)
+				break;
+			else if (*(b + i + a) >= 31 && *(b + i + a))
+				printf("%c", *(b + i + a));
+			else
+				printf(".");
+		}
+		if (a >= size)
+			continue;
+		printf("\n");
+	}
+	if (size <= 0)
+		printf("\n");
+}
