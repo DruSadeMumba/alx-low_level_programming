7bcf9d3f03060b35254955d4c77ca2af0e3944c8
Dru <drusademumba@gmail.com>
2023-05-04 10:54:39 -0700

    Replace letters with numbers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,26 @@
 #include "main.h"
+
+/**
+ * leet - replace characters
+ *
+ * @c: char
+ * Return: char
+ */
+char *leet(char *c)
+{
+	int i, j;
+	char a[] = "aeotlAEOTL";
+	char b[] = "4307143071";
+
+	for (i = 0; *(c + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == c[i])
+			{
+				c[i] = b[j];
+			}
+		}
+	}
+	return (c);
+}
