5ad0d9d577d613c89312d06afeb653a416ff0cf8
Dru <drusademumba@gmail.com>
2023-05-04 08:33:57 -0700

    Add function that changes string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,23 @@
 #include "main.h"
+
+/**
+ * string_toupper - change string to uppercase
+ *
+ * @a: char
+ *
+ * Return: char
+ */
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (*(a + i))
+	{
+		if (*(a + i) >= 'a' && *(a + i) <= 'z')
+		{
+			*(a + i) = *(a + i) - 32;
+		}
+		i++;
+	}
+	return (a);
+}
