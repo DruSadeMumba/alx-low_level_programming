5892c4485c7b6256a0f2af20eee7885af6bf7028
Dru <drusademumba@gmail.com>
2023-05-04 07:32:44 -0700

    Write function to reverse array elements

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,20 @@
 #include "main.h"
+
+/**
+ * reverse_array - reverse array
+ *
+ * @a: int
+ * @n: int
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	j = 0;
+	for (i = 0; (i < n / 2); i++)
+	{
+		j = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = j;
+	}
+}
