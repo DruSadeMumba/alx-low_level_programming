2c8bfdaa7b2df51a505c7babe8a8412b22b9d88d
Dru <drusademumba@gmail.com>
2023-05-12 03:09:43 -0700

    correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -38,17 +38,15 @@ int main(int argc, char *argv[])
 
 	mul = 0;
 
-	if (argc > 2)
-	{
-		x = atoi(argv[1]);
-		y = atoi(argv[2]);
-
-		mul = x * y;
-		printf("%d\n", mul);
-	} else
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	mul = x * y;
+	printf("%d\n", mul);
+
 	return (0);
 }
