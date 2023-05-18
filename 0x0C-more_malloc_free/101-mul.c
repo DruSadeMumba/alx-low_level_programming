17f2bdd519764213a7f24751f87b846a87232941
Dru <drusademumba@gmail.com>
2023-05-18 14:58:48 -0700

    Write a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,35 +1,62 @@
 #include "main.h"
 
 /**
+ * error - error
+ */
+void error(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
  * main - multiply 2 ints
  *
- * @argc: arguments
+ * @argc: arguements
  * @argv: vector
  *
  * Return: int
  */
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int i, j;
+	char *s1, *s2;
+	int len1, len2, l, i, c, x, y, *ans, n = 0;
 
-	if (argc != 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !isdigit(*s1) || !isdigit(*s2))
+		error();
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	l = len1 + len2 + 1;
+	ans = malloc(sizeof(int) * l);
+	if (!ans)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		ans[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		x = s1[len1] - '0';
+		c = 0;
+		for (len2 = len2 - 1; len2 >= 0; len2--)
 		{
-			if (argv[i][j] < 48 || argv[i][j] > 57)
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			y = s2[len2] - '0';
+			c += ans[len1 + len2 + 1] + (x * y);
+			ans[len1 + len2 + 1] = c % 10;
+			c /= 10;
 		}
+		if (c > 0)
+			ans[len1 + len2 + 1] += c;
 	}
-	mul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", mul);
+	for (i = 0; i < l - 1; i++)
+	{
+		if (ans[i])
+			n = 1;
+		if (n)
+			_putchar(ans[i] + '0');
+	}
+	if (!n)
+		_putchar('0');
+	_putchar('\n');
+	free(ans);
 	return (0);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <string.h>
+#include <ctype.h>
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
