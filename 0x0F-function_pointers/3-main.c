e808f24d89d461c58926525e0d08890224c94526
Dru <drusademumba@gmail.com>
2023-05-24 10:37:23 -0700

    Write a program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,1 +1,41 @@
-#include "function_pointers.h"
+#include "3-calc.h"
+
+/**
+ * main - operator
+ *
+ * @argc: num of args
+ * @argv: vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int x, y;
+	char op;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[3]);
+	func = get_op_func(argv[2]);
+	op = *argv[2];
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((op == 37 || op == 47) && y == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", func(x, y));
+	return (0);
+}
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,1 +1,56 @@
-#include "function_pointers.h"
+#include "3-calc.h"
+
+/**
+ * op_add - add
+ * @a: first int
+ * @b: second int
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract
+ * @a: first int
+ * @b: second int
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply
+ * @a: first int
+ * @b: second int
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide
+ * @a: first int
+ * @b: second int
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ * @a: first int
+ * @b: second int
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
