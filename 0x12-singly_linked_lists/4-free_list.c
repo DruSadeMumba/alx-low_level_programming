5a9fbaccee1d1bca6da8479a5db129b99a5422d4
Dru <drusademumba@gmail.com>
2023-06-02 08:38:16 -0700

    Write a function that frees a list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,1 +1,18 @@
 #include "lists.h"
+
+/**
+ * free_list - free list
+ * 
+ * @head: pointer
+ */
+void free_list(list_t *head)
+{
+	list_t *list;
+	while (head)
+	{
+		list = head->next;
+		free(head->str);
+		free(head);
+		head = list;
+	}
+}
