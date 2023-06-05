a95431b22b841496edfa144a4eb91a6232366971
Dru <drusademumba@gmail.com>
2023-06-05 10:33:48 -0700

    Write a function that frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,15 +7,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *list, *ahead = *head;
+	listint_t *list;
 
 	if (!head)
 		return;
-	while (ahead)
+	while (*head)
 	{
-		list = ahead->next;
-		free(ahead);
-		ahead = list;
+		list = (*head)->next;
+		free(*head);
+		*head = list;
 	}
 	*head = NULL;
 }
