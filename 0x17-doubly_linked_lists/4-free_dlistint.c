c6a4a6471fc573022f99c46daacae3885e71cf1e
Dru Sade Mumba <drusademumba@gmail.com>
2023-08-17 05:34:39 -0700

    Write a function that frees a dlistint_t list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,1 +1,16 @@
 #include "lists.h"
+
+/**
+ * free_dlistint - free list
+ * @head: pointer
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *h = NULL;
+	while (head)
+	{
+		h = head->next;
+		free(head);
+		head = h;
+	}
+}
