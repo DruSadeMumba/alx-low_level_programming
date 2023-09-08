0b3e5d17ea636e73e65f5888f7bc5a5b57f03398
Dru Sade Mumba <drusademumba@gmail.com>
2023-09-08 07:46:56 -0700

    Write a function that retrieves a value associated with a key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,1 +1,45 @@
 #include "hash_tables.h"
+
+/**
+ * hash_table_set - add element
+ * @ht: pointer
+ * @key: key
+ * @value: value
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *copy = strdup(value);
+	unsigned long n, i;
+
+	if (!ht || !key || !value || !copy)
+		return (0);
+
+	n = key_index((const unsigned char *)key, ht->size);
+	for (i = n; ht->array[i]; i++)
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = copy;
+			return (1);
+		}
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+	{
+		free(copy);
+		return (0);
+	}
+
+	new->key = strdup(key);
+	if (!new->key)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = copy;
+	new->next = ht->array[n];
+	ht->array[n] = new;
+
+	return (1);
+}
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,1 +1,25 @@
 #include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieve value
+ * @ht: pointer
+ * @key: key
+ * Return: value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long n;
+	hash_node_t *new;
+
+	if (!ht || !key)
+		return (NULL);
+	n = key_index((const unsigned char *)key, ht->size);
+
+	new = (ht->array)[n];
+	if (!new)
+		return (NULL);
+	else
+		return (new->value);
+	while (new && strcmp(new->key, key) != 0)
+		new = new->next;
+}
