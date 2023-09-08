1c8e8b3c64983ffa2d73285d6851bc838e301f37
Dru Sade Mumba <drusademumba@gmail.com>
2023-09-08 06:53:46 -0700

    Add function

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,1 +1,26 @@
 #include "hash_tables.h"
+
+/**
+ * shash_table_create - create sorted hash table
+ * @size: array size
+ * Return: pointer
+ */
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	shash_table_t *sht;
+	unsigned long int i;
+
+	sht = malloc(sizeof(shash_table_t));
+	if (!sht)
+		return (NULL);
+
+	sht->size = size;
+	sht->array = malloc(sizeof(shash_node_t *) * size);
+
+	if (!sht->array)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		sht->array[i] = NULL;
+
+	return (sht);
+}
