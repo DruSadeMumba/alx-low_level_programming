b5d11a7b6278af22e38f4f9ef31135c20a743bd7
Dru Sade Mumba <drusademumba@gmail.com>
2023-09-08 07:55:15 -0700

    correction

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long n;
 	hash_node_t *new;
 
-	if (!ht || !key)
+	if (!ht || !key || *key == '\0')
 		return (NULL);
 	n = key_index((const unsigned char *)key, ht->size);
 
